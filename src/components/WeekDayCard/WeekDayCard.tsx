995cd25225155c4fe5c7884d44dc10b13fe42df3
GustavoCedisa <gustavo.dias@cedisa.com.br>
2023-08-25 17:27:29 -0300

    feat: add WeekDayCard in settigns page

---
+++ src/components/WeekDayCard/WeekDayCard.tsx
@@ -0,0 +1,16 @@
+import styles from './WeekDayCard.module.css'
+
+export interface WeekDayCardProps {
+    weekday: string
+    training: string
+}
+
+export const WeekDayCard = ({ weekday, training }: WeekDayCardProps) => {
+
+    return (
+        <div className={styles['card']}>
+            <span className={styles['weekday']}>{weekday}</span>
+            <span className={styles['training']}>{training}</span>
+        </div>
+    );
+}
--- src/pages/settings/id.tsx
+++ src/pages/settings/id.tsx
@@ -1,11 +1,35 @@
 import { Header } from '@/components/UI/Header/Header';
 import { SEO } from '@/components/SEO/SEO';
 import Image from 'next/image';
-import { TrainingSection } from '@/components/TrainingSection/TrainingSection';
 import Link from 'next/link';
 import { LateralMenu } from '@/components/LateralMenu/LateralMenu';
+import styles from '@/styles/Settings.module.css';
+import { WeekDayCard } from '@/components/WeekDayCard/WeekDayCard';
 
-export default function Home() {
+export default function Settings() {
+    const trainings = [{
+        weekday: 'Segunda',
+        training: 'Peito',
+    }, {
+        weekday: 'Segunda',
+        training: 'Peito',
+    }, {
+        weekday: 'Segunda',
+        training: 'Peito',
+    }, {
+        weekday: 'Segunda',
+        training: 'Peito',
+    }, {
+        weekday: 'Segunda',
+        training: 'Peito',
+    }, {
+        weekday: 'Segunda',
+        training: 'Peito',
+    }, {
+        weekday: 'Segunda',
+        training: 'Peito',
+    }];
+
     return (
         <>
             <SEO />
@@ -17,7 +41,17 @@ export default function Home() {
                 ]}
             />
 
-            <LateralMenu components={[<span>Treinos</span>, <span>Configuração de Conta</span>]} />
+
+            <section className={styles['fl-settings']}>
+                <LateralMenu components={[<span>Treinos</span>, <span>Configuração de Conta</span>]} />
+                <section className={styles['fl-setting-section']}>
+                    <h3 className={styles['fl-settings-header']}>Treinos</h3>
+
+                    <div className={styles['fl-setting-weekdyas']}>
+                        {trainings.map((item) => <WeekDayCard weekday={item['weekday']} training={item['training']} />)}
+                    </div>
+                </section>
+            </section>
         </>
     );
 }
