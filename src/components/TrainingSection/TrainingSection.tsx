de913359df11918031555b4a83e07aff26dccffc
GustavoCedisa <gustavo.dias@cedisa.com.br>
2023-09-04 15:35:55 -0300

    feat: add training plan title/name in home and start work in dropdown titles/name for all training plans

--- src/components/TrainingSection/TrainingSection.tsx
+++ src/components/TrainingSection/TrainingSection.tsx
@@ -1,6 +1,7 @@
 import { useState, MouseEvent } from 'react';
 import Link from 'next/link';
 import styles from '@/components/TrainingSection/TrainingSection.module.css';
+import Image from 'next/image';
 
 export const TrainingSection = () => {
     const [startIndex, setStartIndex] = useState(0);
@@ -32,36 +33,61 @@ export const TrainingSection = () => {
     }
 
     return (
-        <section className={`${styles['fd-training-section']}`}>
-            <div className={`${styles['fd-weekdays']}`}>
-                {/* <h3 className={`${styles['fd-title']}`}>Dias da Semana</h3> */}
-                {daysOfWeek.slice(startIndex, startIndex + maxVisibleSpans).map((day, index) => (
-                    <span key={index} className={`${styles['fd-days']} ${styles[`i-${index}`]}`} onClick={(e) => handleSpanClick(e, day, index)} title={day}>{day}</span>
-                ))}
-            </div>
+        <>
+            <section className={`${styles['fd-training-title']}`}>
+                <div className={`${styles['fd-dropdown']}`}>
+                    <div className={styles['fd-dropdown-training']}>
+                        <h3 className={`${styles['fd-dropdown-training-title']}`}>Título do Treino</h3>
+                        <Image src="/images/more.svg" alt='More Icon - Open more training plans' width={25} height={25}/>
+                    </div>
 
-            <div className={`${styles['exercism-plan']}`}>
-                <div className={`${styles['title']}`}>
-                    <h3 className={`${styles['fd-title']}`}>Treino de peito</h3>
-                    <div className={`${styles['play-btn']}`} title='Começar treino'>
-                        <img src="/images/play.svg" alt="" />
+                    <ul className={`${styles['fd-dropdown-title-list']}`}>
+                        <li className={`${styles['fd-dropdown-ttitle-item']}`}>Título do Treino 1</li>
+                        <li className={`${styles['fd-dropdown-ttitle-item']}`}>Título do Treino 2</li>
+                        <li className={`${styles['fd-dropdown-ttitle-item']}`}>Título do Treino 3</li>
+                        <li className={`${styles['fd-dropdown-ttitle-item']}`}>Título do Treino 4</li>
+                        <li className={`${styles['fd-dropdown-ttitle-item']}`}>Título do Treino 5</li>
+                        <li className={`${styles['fd-dropdown-ttitle-item']}`}>Título do Treino 5</li>
+                        <li className={`${styles['fd-dropdown-ttitle-item']}`}>Título do Treino 5</li>
+                        <li className={`${styles['fd-dropdown-ttitle-item']}`}>Título do Treino 5</li>
+                    </ul>
+                </div>
+            </section>
+            <section className={`${styles['fd-training-section']}`}>
+                <div className={`${styles['fd-weekdays']}`}>
+                    {/* <h3 className={`${styles['fd-title']}`}>Dias da Semana</h3> */}
+                    {daysOfWeek.slice(startIndex, startIndex + maxVisibleSpans).map((day, index) => (
+                        <span key={index} className={`${styles['fd-days']} ${styles[`i-${index}`]}`} onClick={(e) => handleSpanClick(e, day, index)} title={day}>{day}</span>
+                    ))}
+                </div>
+
+                <div className={`${styles['exercism-day-list']}`}>
+
+                    <div className={`${styles['title']}`}>
+                        <h3 className={`${styles['fd-title']}`}>Treino de peito</h3>
+                        <div className={`${styles['play-btn']}`} title='Começar treino'>
+                            <img src="/images/play.svg" alt="" />
+                        </div>
+                    </div>
+
+                    <div className={`${styles['exercism-plan']}`}>
+                        <div className={`${styles['exercism']}`}>
+                            <span className={`${styles['fd-exer']} ${styles['fd-exer-selected']}`}>Flexão declinada / 3x8 - 30s off</span>
+                            <span className={`${styles['fd-exer']}`}>Flexão declinada / 3x8 - 30s off</span>
+                            <span className={`${styles['fd-exer']}`}>Flexão declinada / 3x8 - 30s off</span>
+                            <span className={`${styles['fd-exer']}`}>Flexão declinada / 3x8 - 30s off</span>
+                            <span className={`${styles['fd-exer']}`}>Flexão declinada / 3x8 - 30s off</span>
+                            <span className={`${styles['fd-exer']}`}>Flexão declinada / 3x8 - 30s off</span>
+                            <span className={`${styles['fd-exer']}`}>Flexão declinada / 3x8 - 30s off</span>
+                            <span className={`${styles['fd-exer']}`}>Flexão declinada / 3x8 - 30s off</span>
+                            <span className={`${styles['fd-exer']}`}>Flexão declinada / 3x8 - 30s off</span>
+                            <span className={`${styles['fd-exer']}`}>Flexão declinada / 3x8 - 30s off</span>
+                            <span className={`${styles['fd-exer']}`}>Flexão declinada / 3x8 - 30s off</span>
+                            <span className={`${styles['fd-exer']}`}>Flexão declinada / 3x8 - 30s off</span>
+                        </div>
                     </div>
                 </div>
-                <div className={`${styles['exercism']}`}>
-                    <span className={`${styles['fd-exer']}`}>Segunda</span>
-                    <span className={`${styles['fd-exer']}`}>Segunda</span>
-                    <span className={`${styles['fd-exer']}`}>Segunda</span>
-                    <span className={`${styles['fd-exer']}`}>Segunda</span>
-                    <span className={`${styles['fd-exer']}`}>Segunda</span>
-                    <span className={`${styles['fd-exer']}`}>Segunda</span>
-                    <span className={`${styles['fd-exer']}`}>Segunda</span>
-                    <span className={`${styles['fd-exer']}`}>Segunda</span>
-                    <span className={`${styles['fd-exer']}`}>Segunda</span>
-                    <span className={`${styles['fd-exer']}`}>Segunda</span>
-                    <span className={`${styles['fd-exer']}`}>Segunda</span>
-                    <span className={`${styles['fd-exer']}`}>Segunda</span>
-                </div>
-            </div>
-        </section>
+            </section>
+        </>
     )
 }
