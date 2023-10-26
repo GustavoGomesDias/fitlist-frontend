9fe7713c2e75754fc4f5ca0b189f01de1253f81e
GustavoCedisa <gustavo.dias@cedisa.com.br>
2023-10-26 13:21:30 -0300

    feat: add correct url push

--- src/pages/home/[id].tsx
+++ src/pages/home/[id].tsx
@@ -5,17 +5,22 @@ import { TrainingSection } from '@/components/TrainingSection/TrainingSection';
 import { useRouter } from 'next/router';
 import { GetServerSideProps } from 'next';
 import { parseCookies } from 'nookies';
+import { useCookies } from '@/hooks';
+import { UserInfo } from '@/context/types';
 
 export default function Home() {
     const { push } = useRouter();
-
+    const { getCookie } = useCookies();
 
     return (
         <>
             <SEO />
             <Header
                 navItemComponents={[
-                    <Image src="/images/settings.svg" alt="Botão de configurações" width="30" height="30" key="/images/settings.svg"onClick={() => push('/settings/id')}  />
+                    <Image src="/images/settings.svg" alt="Botão de configurações" width="30" height="30" key="/images/settings.svg" onClick={() => {
+                        const user = JSON.parse(getCookie('user')) as UserInfo;
+                        push('/settings/[id]', `/settings/${user.id}`)
+                    }}  />
                 ]}
             />
 
